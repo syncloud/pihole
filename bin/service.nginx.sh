947c9530dd3cb8352cdf2393073a14e78d6942cc
Boris Rybalkin <ribalkin@gmail.com>
2019-09-10 20:21:08 +0100

    remove prestart

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -7,15 +7,10 @@ if [[ -z "$1" ]]; then
     exit 1
 fi
 
-#SOCKET=${SNAP_DATA}/.socket
-
 case $1 in
-pre-start)
-    #timeout 600 /bin/bash -c 'until [ -S '${SOCKET}' ]; do echo "waiting for ${SOCKET}"; sleep 1; done'
+start)
     /bin/rm -f ${SNAP_COMMON}/web.socket
     exec ${DIR}/nginx/sbin/nginx -t -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
-    ;;
-start)
     exec ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
     ;;
 reload)
