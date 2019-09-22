a105b043d3dcaa303c2ea18f5da72dc2c3729151
Boris Rybalkin <ribalkin@gmail.com>
2019-09-22 20:43:24 +0100

    fix adlists

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ cd pi-hole-feature-api
 sed -i 's#/etc/pihole#/var/snap/pihole/common/etc/pihole#g' gravity.sh
 sed -i 's#piholeDir="/etc/${basename}"#piholeDir="/var/snap/pihole/common/etc/pihole"#g' gravity.sh
 sed -i 's#piholeGitDir=.*#piholeGitDir="/snap/pihole/current"#g' gravity.sh
-sed -i 's#/opt/pihole/#/snap/pihole/current/advanced/Scripts"#g' gravity.sh
+sed -i 's#/opt/pihole#/snap/pihole/current/advanced/Scripts"#g' gravity.sh
 sed -i 's#sqlite3#/snap/pihole/current/sqlite/bin/sqlite3#g' gravity.sh
 cp gravity.sh ${BUILD_DIR}/bin
 cp -r advanced ${BUILD_DIR}/
