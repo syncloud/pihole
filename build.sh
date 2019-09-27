777bcf80273a56c58fbfde30e4c578dcde8a8c77
Boris Rybalkin <ribalkin@gmail.com>
2019-09-27 08:49:11 +0100

    fix pihole dit

--- build.sh
+++ build.sh
@@ -60,6 +60,7 @@ sed -i 's#piholeGitDir=.*#piholeGitDir="/snap/pihole/current"#g' gravity.sh
 sed -i 's#/opt/pihole#/snap/pihole/current/advanced/Scripts#g' gravity.sh
 sed -i 's#sqlite3#/snap/pihole/current/sqlite/bin/sqlite3#g' gravity.sh
 sed -i 's#sqlite3#/snap/pihole/current/sqlite/bin/sqlite3#g' advanced/Scripts/database_migration/gravity-db.sh
+sed -i 's#/etc/.pihole#/snap/pihole/current#g' advanced/Scripts/database_migration/gravity-db.sh
 sed -i 's#dig#/snap/pihole/current/bind9/bin/dig#g' gravity.sh
 sed -i 's#PIHOLE_COMMAND=.*#PIHOLE_COMMAND=true#g' gravity.sh
 cp gravity.sh ${BUILD_DIR}/bin
