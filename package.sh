bce01851a1583037262387da6ee1454680eb9d8f
Boris Rybalkin <ribalkin@gmail.com>
2021-03-17 08:01:48 +0000

    replace killall with pkill

--- package.sh
+++ package.sh
@@ -43,8 +43,9 @@ find . -regex "\(.*.sh\|.*pihole\)" -exec sed -i 's#/opt/pihole#/snap/pihole/cur
 find . -regex "\(.*.sh\|.*pihole\)" -exec sed -i 's#sqlite3#/snap/pihole/current/bin/sqlite.sh#g' {} +
 find . -regex "\(.*.sh\|.*pihole\)" -exec sed -i 's#dig#/snap/pihole/current/bind9/bin/dig.sh#g' {} +
 find . -regex "\(.*.sh\|.*pihole\)" -exec sed -i 's#PIHOLE_COMMAND=.*#PIHOLE_COMMAND=true#g' {} +
+find . -regex "\(.*.sh\|.*pihole\)" -exec sed -i 's#/usr/local/bin#/snap/pihole/current/bin#g' {} +
+find . -regex "\(.*.sh\|.*pihole\)" -exec sed -i 's#killall -q#pkill -f#g' {} +
 sed -i 's#lsof -Pni:53#snap services pihole.ftl | tail -1 | grep active#g' pihole
-sed -i 's#/usr/local/bin#/snap/pihole/current/bin#g' pihole
 
 cp gravity.sh ${BUILD_DIR}/bin
 cp pihole ${BUILD_DIR}/bin
