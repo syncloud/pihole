36aa767f4c736860ad2256b57340c4a2bc640a94
Boris Rybalkin <ribalkin@gmail.com>
2021-03-19 08:56:55 +0000

    main ui test

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -43,6 +43,12 @@ def _test_login(selenium, mode, device_user, device_password):
     selenium.screenshot('login-submitted-' + mode)
 
 
+
+def test_main(selenium):
+    blocked_size = int(selenium.find_by_id("domains_being_blocked").text)
+    selenium.screenshot('main')
+    assert blocked_size > 0
+
 def test_whitelist(selenium):
     selenium.find_by_xpath("//span[text()='Whitelist']").click()
     selenium.screenshot('whitelist')
