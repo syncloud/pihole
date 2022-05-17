c11b95dd49687b5b7ce12f82bab577885a9b402a
Boris Rybalkin <ribalkin@gmail.com>
2022-05-17 21:24:28 +0100

    php

--- bin/service.php-fpm.sh
+++ bin/service.php-fpm.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-exec DIR/php/bin/php-fpm.sh --allow-to-run-as-root -y ${SNAP_DATA}/config/php-fpm.conf -c ${SNAP_DATA}/config/php.ini
+exec $DIR/php/bin/php-fpm.sh --allow-to-run-as-root -y ${SNAP_DATA}/config/php-fpm.conf -c ${SNAP_DATA}/config/php.ini
