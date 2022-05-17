5965c0781218c64d5b70ef9ce08f00bb9e55989e
Boris Rybalkin <ribalkin@gmail.com>
2022-05-17 21:06:39 +0100

    ftl config

--- bin/service.ftl.sh
+++ bin/service.ftl.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-exec $DIR/FTL/bin/pihole-FTL -C $DIR/config/pihole/dnsmasq.conf debug
+exec $DIR/FTL/bin/pihole-FTL -- -C $DIR/config/pihole/dnsmasq.conf
