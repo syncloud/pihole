67dab13a3b2fffa1f391163981a46825a0c2cf4d
Boris Rybalkin <ribalkin@gmail.com>
2022-05-18 22:15:35 +0100

    clear shm

--- bin/service.ftl.sh
+++ bin/service.ftl.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-
+rm -rf /dev/shm/FTL*
 exec $DIR/FTL/bin/pihole-FTL debug
--- -C /var/snap/pihole/current/config/01-pihole.conf
