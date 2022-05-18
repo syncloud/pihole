8894a6a287c0fcdaf72049dafbd189705499b932
Boris Rybalkin <ribalkin@gmail.com>
2022-05-18 22:07:13 +0100

    conf

--- bin/service.ftl.sh
+++ bin/service.ftl.sh
@@ -2,4 +2,5 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-exec $DIR/FTL/bin/pihole-FTL debug -- -C /var/snap/pihole/current/config/01-pihole.conf
+exec $DIR/FTL/bin/pihole-FTL debug
+-- -C /var/snap/pihole/current/config/01-pihole.conf
