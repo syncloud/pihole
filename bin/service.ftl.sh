1f7096edcf2f7597dfecf1ade1f220c398c760ad
Boris Rybalkin <ribalkin@gmail.com>
2022-05-19 21:17:33 +0100

    fix ftl config

--- bin/service.ftl.sh
+++ bin/service.ftl.sh
@@ -2,4 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 rm -rf /dev/shm/FTL*
-exec $DIR/FTL/bin/pihole-FTL debug
+exec $DIR/FTL/bin/pihole-FTL debug -- -C /var/snap/pihole/current/config/dnsmasq.conf
