1da173e6b6987c3762877221c3391e9226704e7b
Boris Rybalkin <ribalkin@gmail.com>
2023-03-26 22:00:12 +0100

    fix netcat

--- netcat/build.sh
+++ netcat/build.sh
@@ -21,6 +21,6 @@ mkdir ${BUILD_DIR}/lib
 cp $DIR/build/lib/*-linux*/ld-*.so ${BUILD_DIR}/lib/ld.so
 cp $DIR/build/lib/*-linux*/libc.so* ${BUILD_DIR}/lib
 mkdir ${BUILD_DIR}/bin
-cp $DIR/build/bin/nc ${BUILD_DIR}/bin/nc
+cp $DIR/build/bin/nc ${BUILD_DIR}/bin
 cp ${DIR}/nc.sh ${BUILD_DIR}/bin
 ${BUILD_DIR}/bin/nc.sh -h
