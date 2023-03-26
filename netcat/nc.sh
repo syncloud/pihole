df06f6a83407d2dc39fbd52eba4f956bb8dadc3e
Boris Rybalkin <ribalkin@gmail.com>
2023-03-26 22:04:27 +0100

    fix bin

--- netcat/nc.sh
+++ netcat/nc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 DIR=$( cd "$( dirname "$0" )" && cd .. && pwd )
-${DIR}/lib/ld.so --library-path ${DIR}/lib ${DIR}/bin/nc.bin "$@"
+${DIR}/lib/ld.so --library-path ${DIR}/lib ${DIR}/bin/nc "$@"
