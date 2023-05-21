7bcad946033b61e7ab72bea529019a7e4bc226ee
Boris Rybalkin <ribalkin@gmail.com>
2023-05-21 09:30:05 +0100

    fix

--- build.sh
+++ build.sh
@@ -62,8 +62,9 @@ sed -i 's#IPv6\.\*TCP#tcp6#g' pihole
 cp gravity.sh ${BUILD_DIR}/bin
 cp pihole ${BUILD_DIR}/bin
 cp -r advanced ${BUILD_DIR}
-ln -s /snap/pihole/current/bin/gravity.sh ${BUILD_DIR}/advanved/Scripts/gravity.sh
 cp -r "automated install" ${BUILD_DIR}
+cd ${BUILD_DIR}/advanved/Scripts
+ln -s /snap/pihole/current/bin/gravity.sh gravity.sh
 #cp advanced/dnsmasq.conf.original ${BUILD_DIR}/config.templates/dnsmasq.conf
 #cp advanced/01-pihole.conf ${BUILD_DIR}/config.templates/01-pihole.conf
 
