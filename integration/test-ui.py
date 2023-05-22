ded8691964d4343db35bfafed71054dfbcabc8dc
Boris Rybalkin <ribalkin@gmail.com>
2023-05-22 06:38:05 +0100

    adlist ui test

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -87,6 +87,11 @@ def test_local_dns(selenium, device, device_host, ui_mode):
     #assert '1.1.1.1' in output
 
 
+def test_adlists(selenium, device, device_host, ui_mode):
+    selenium.find_by_xpath("//a[contains(.,'Adlists')]").click()
+    selenium.find_by_xpath("//a[contains(.,'Update')]").click()
+    
+
 def test_teardown(driver):
     driver.quit()
 
