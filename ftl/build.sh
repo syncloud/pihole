c4a8cc0c20e7517a87768a31068cbbbf09da388b
Boris Rybalkin <ribalkin@gmail.com>
2026-07-12 07:29:06 +0100

    v6-port ftl-src cycle: build nettle 3.9, bundle FTL libs + loader
    bookworm nettle 3.8 lacks nettle/balloon.h required by FTL v6. Build nettle 3.9
    to /opt/nettle and point FTL cmake at it via CMAKE_PREFIX_PATH. From-source FTL
    is dynamically linked, so bundle its ldd libs + ld.so and run via a loader
    wrapper (same technique as nginx/bind9).

--- ftl/build.sh
+++ ftl/build.sh
@@ -2,14 +2,25 @@
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
 FTL_VERSION=v6.7
+NETTLE_VERSION=3.9
 
 apt-get update
-apt-get install -y wget build-essential cmake git m4 nettle-dev libgmp-dev libidn2-dev libunistring-dev libreadline-dev xxd
+apt-get install -y wget build-essential cmake git m4 libgmp-dev libidn2-dev libunistring-dev libreadline-dev xxd
 
 BUILD_DIR=${DIR}/../build/snap/FTL
-mkdir -p ${BUILD_DIR}/bin
+mkdir -p ${BUILD_DIR}/bin ${BUILD_DIR}/lib
 
 cd ${DIR}/../build
+
+# nettle 3.9 (bookworm ships 3.8 which lacks nettle/balloon.h that FTL v6 needs)
+wget --progress=dot:giga https://ftp.gnu.org/gnu/nettle/nettle-${NETTLE_VERSION}.tar.gz
+tar xf nettle-${NETTLE_VERSION}.tar.gz
+cd nettle-${NETTLE_VERSION}
+./configure --prefix=/opt/nettle
+make -j$(nproc)
+make install
+cd ${DIR}/../build
+
 wget --progress=dot:giga https://github.com/pi-hole/FTL/archive/${FTL_VERSION}.tar.gz
 tar xf ${FTL_VERSION}.tar.gz
 mv FTL-* FTL-src
@@ -24,6 +35,21 @@ grep -rlE '/etc/pihole|/var/log/pihole|/run/pihole-FTL.pid' src | xargs sed -i \
 # enable CivetWeb unix-domain-socket listening ("x<path>" port syntax)
 sed -i 's/TIMER_RESOLUTION=1000/TIMER_RESOLUTION=1000\n    USE_X_DOM_SOCKET/' src/webserver/civetweb/CMakeLists.txt
 
+export CMAKE_PREFIX_PATH=/opt/nettle
 bash build.sh
-mv pihole-FTL ${BUILD_DIR}/bin/pihole-FTL
+mv pihole-FTL ${BUILD_DIR}/bin/pihole-FTL.bin
+
+# bundle shared libs + loader (from-source FTL is dynamically linked)
+for lib in $(ldd ${BUILD_DIR}/bin/pihole-FTL.bin | grep '=>' | awk '{print $3}'); do
+  cp -L ${lib} ${BUILD_DIR}/lib/
+done
+cp -L $(ldd ${BUILD_DIR}/bin/pihole-FTL.bin | grep 'ld-linux' | awk '{print $1}') ${BUILD_DIR}/lib/ld.so
+
+cat > ${BUILD_DIR}/bin/pihole-FTL <<'EOF'
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+exec ${DIR}/lib/ld.so --library-path ${DIR}/lib ${DIR}/bin/pihole-FTL.bin "$@"
+EOF
+chmod +x ${BUILD_DIR}/bin/pihole-FTL
+
 ${BUILD_DIR}/bin/pihole-FTL --version
